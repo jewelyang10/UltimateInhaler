943273cdb87ad3b17cf532e2213ded1c6d3160c4
XiaochuanWang <potter3186@msn.com>
2016-09-22 08:43:59 +1000

    modify UI and add various reading methods of DB

--- app/src/main/java/monash/ultimateinhaler/PredictionFragment.java
+++ app/src/main/java/monash/ultimateinhaler/PredictionFragment.java
@@ -17,14 +17,11 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -67,56 +64,29 @@ public class PredictionFragment extends Fragment {
 
     }
 
-    public static int sigmoidConvert(ArrayList<Integer> base) {
-        int range = Collections.max(base) - Collections.min(base);
-
-        for (int singleItem : base) {
-            double result = (singleItem - Collections.min(base)) / range;
-        }
-        return 0;
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         setHasOptionsMenu(true);
 
-        Bundle rawData = this.getArguments();
-
         //set up basic layout
         rootview = inflater.inflate(R.layout.fragment_prediction, container, false);
         WebView predictBase = (WebView) rootview.findViewById(R.id.predictBaseContainer);
 
-        //get GPS location
-
-
-        //get Weather
-
-
         //interactive class between java and javascript
         class WebAppInterface {
             Context mContext;
-            Bundle RawVars;
 
-            String LTP = "[12, 19, 25, 5]";
-            String pollen =  "[90, 70, 50, 25, 30, 40, 80]";
-            String OD =  "[25, 5, 70]";
+
+            String LTP = null;
+            String pollen = null;
+            String OD = null;
 
             /** Instantiate the interface and set the context */
-            WebAppInterface(Context c, Bundle realData) {
+            WebAppInterface(Context c) {
                 mContext = c;
-                RawVars = realData;
 
-                if (RawVars != null)
-                {
-                    LTP = RawVars.getString("LTP" );
-                    pollen =RawVars.getString("pollen");
-                    OD = RawVars.getString("OD");
-
-                }else
-                {
-                    Toast.makeText(getContext(), "Prediction report is still under development...", Toast.LENGTH_LONG).show();
-                }
             }
 
             //further development required after the buddle issue
@@ -135,11 +105,7 @@ public class PredictionFragment extends Fragment {
             {
                 return this.OD;
             }
-            @JavascriptInterface
-            public void clickDate(String date)
-            {
-                Toast.makeText( getContext() , "date: "+date+" are clicked and i need a buddle from Jewel to continue!" , Toast.LENGTH_LONG).show();
-            }
+
         }
 
         WebSettings webSettings = predictBase.getSettings();
@@ -149,7 +115,7 @@ public class PredictionFragment extends Fragment {
         predictBase.setWebChromeClient(new WebChromeClient());
         predictBase.setWebViewClient(new WebViewClient());
 
-        predictBase.addJavascriptInterface(new WebAppInterface(rootview.getContext(),rawData), "Android");
+        predictBase.addJavascriptInterface(new WebAppInterface(rootview.getContext()), "Android");
         predictBase.loadUrl("file:///android_asset/predictionpage.html");
         return rootview;
     }
@@ -164,12 +130,8 @@ public class PredictionFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_exit_diary) {
-            // Eliminar todos los leads
-//            mLeadsAdapter.clear();
-
             MoreFragment fragment6 = new MoreFragment();
-            FragmentTransaction fragmentTransaction6 =
-                    getFragmentManager().beginTransaction();
+            FragmentTransaction fragmentTransaction6 = getFragmentManager().beginTransaction();
             fragmentTransaction6.replace(R.id.fragment_containerStart, fragment6);
             fragmentTransaction6.addToBackStack(null);
             fragmentTransaction6.commit();
--- app/src/main/java/monash/ultimateinhaler/autoRecord/databaseWrite.java
+++ app/src/main/java/monash/ultimateinhaler/autoRecord/databaseWrite.java
@@ -2,6 +2,7 @@ package monash.ultimateinhaler.autoRecord;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -15,6 +16,10 @@ import java.util.Date;
  */
 public class databaseWrite extends SQLiteOpenHelper {
 
+    private static final String TableName = "RAWINFO";
+    private static final String Column1_PK = "date_pk";
+    private static final String Column2_weather = "WESTHER";
+    private static final String Column3_pollen = "POLLEN";
     SQLiteDatabase db = null;
 
     public databaseWrite(Context context) {
@@ -32,11 +37,11 @@ public class databaseWrite extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String CREATE_RAWINFO_TABLE = "CREATE TABLE " + "RAWINFO" +
+        String CREATE_RAWINFO_TABLE = "CREATE TABLE " + TableName +
                 "(" +
-                "date_pk" + " TEXT PRIMARY KEY," +
-                "WESTHER" + " BLOB," +
-                "POLLEN" + " BLOB" +
+                Column1_PK + " TEXT PRIMARY KEY," +
+                Column2_weather + " BLOB," +
+                Column3_pollen + " BLOB" +
                 ")";
 
         try {
@@ -57,12 +62,21 @@ public class databaseWrite extends SQLiteOpenHelper {
 
     public Boolean insertData(String Weather, String Pollen) {
         ContentValues singleEntry = new ContentValues();
-
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         singleEntry.put("date_pk", dateFormat.format(new Date()));
         singleEntry.put("WESTHER", Weather);
         singleEntry.put("POLLEN", Pollen);
         long resultNum = db.insert("RAWINFO", null, singleEntry);
         return resultNum != -1;
+    }
+
+    public Cursor getData(Date requestDate) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Cursor res = db.rawQuery("select * from " + TableName + " where " + Column1_PK + " = " + dateFormat.format(requestDate), null);
+        return res;
+    }
+
+    public String getDateBasePath() {
+        return db.getPath();
     }
 }
